224ac0b81f1ee73d6a71f198cf35f68baf77a751
Sayan650 <sayanp650@gmail.com>
2024-12-05 02:19:25 +0530

    basic canvas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import { AgentsTable } from "../components/agents/AgentsTable";
-import { Header } from "../components/dashboard/Header";
-import { Stats } from "../components/dashboard/Stats";
-import { Sidebar } from "../components/layout/Sidebar";
+import Web3BlocksComponentWrapper from "@/components/canvas/whiteboard";
+// import { AgentsTable } from "../components/agents/AgentsTable";
+// import { Header } from "../components/dashboard/Header";
+// import { Stats } from "../components/dashboard/Stats";
 
 export default function Home() {
   {
     return (
-      <div className="flex min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
-        <Sidebar />
+      <div className="flex min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 w-full ">
 
-        <main className="flex-1 p-8">
-          <Header />
+        <main className="flex-1 w-full">
+          {/* <Header />
           <Stats />
 
           <div className="bg-white/5 backdrop-blur-lg rounded-xl p-6">
             <AgentsTable />
-          </div>
+          </div> */}
+          <Web3BlocksComponentWrapper/>
         </main>
       </div>
     );
